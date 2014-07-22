1643f5dcaf622d58bd8b12f94fd6d8b9021f6b9c
GleasonK <kaagleason@gmail.com>
2014-07-22 16:20:02 -0500

    Fixing

--- large_fix.sh
+++ large_fix.sh
@@ -1,1 +1,1 @@
-git filter-branch --tree-filter 'rm -rf `cat /home/saggezza/Idea/IdeaProjects/large_files.txt | cut -d " " -f 2` ' --prune-empty master
+git filter-branch --tree-filter 'rm -rf `cat /home/saggezza/IdeaProjects/large_files.txt | cut -d " " -f 2` ' --prune-empty master
